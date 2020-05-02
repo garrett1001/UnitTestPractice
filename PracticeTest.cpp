2462b2068f73d017306b89ebe731b4c8e35fc5db
garrett1001 <gwelton@mail.csuchico.edu>
2020-05-01 20:31:52 -0700

    added some basic tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -14,9 +14,30 @@ class PracticeTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
+/* Test isPalindrome */
 TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("aa");
     ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_not_a_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Ba");
+    ASSERT_FALSE(actual);
+}
+
+/* Test sortDescending */
+TEST(PracticeTest, simple_sort)
+{
+    Practice obj;
+	int first = 1;
+	int second = 2;
+	int third = 3;
+	obj.sortDescending(first, second, third);
+    ASSERT_EQ(first, 3);
+	ASSERT_EQ(second, 2);
+	ASSERT_EQ(third, 1);
 }
