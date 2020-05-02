caa00869e83ef0d48a817ad8848dce2491c68c3b
garrett1001 <gwelton@mail.csuchico.edu>
2020-05-01 20:56:46 -0700

    Added some more tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -29,6 +29,62 @@ TEST(PracticeTest, is_not_a_palindrome)
     ASSERT_FALSE(actual);
 }
 
+TEST(PracticeTest, is_single_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("a");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_odd_char_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("aba");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_odd_char_not_a_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("Baa");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_empty_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_integer_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("121");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_integer_not_a_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("211");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PracticeTest, is_symbol_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("<($_$)>");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PracticeTest, is_symbol_not_a_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome(">:D");
+    ASSERT_FALSE(actual);
+}
+
 /* Test sortDescending */
 TEST(PracticeTest, simple_sort)
 {
@@ -36,6 +92,18 @@ TEST(PracticeTest, simple_sort)
 	int first = 1;
 	int second = 2;
 	int third = 3;
+	obj.sortDescending(first, second, third);
+    ASSERT_EQ(first, 3);
+	ASSERT_EQ(second, 2);
+	ASSERT_EQ(third, 1);
+}
+
+TEST(PracticeTest, sorted_simple_sort)
+{
+    Practice obj;
+	int first = 3;
+	int second = 2;
+	int third = 1;
 	obj.sortDescending(first, second, third);
     ASSERT_EQ(first, 3);
 	ASSERT_EQ(second, 2);
